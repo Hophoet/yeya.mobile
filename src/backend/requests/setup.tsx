ea9a5877b234ea2dfd8a48fba1fea9fa842af6ce
hophoet <hophoet@gmail.com>
2021-08-15 20:55:02 +0000

    (backend, setup) add backend core urls

---
+++ src/backend/requests/setup.tsx
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const HOST ='http://127.0.0.1:8000';//'https://byiapi.herokuapp.com';
+
+// Authentication urls 
+const SIGN_UP_URL = `${HOST}/auth/register`;
+const SIGN_IN_URL = `${HOST}/auth/jwt/login`;
+const SEND_PASSWORD_RESET_CODE_URL = `${HOST}/user/reset-pasword`;
+const VERIFIY_PASSWORD_RESET_CODE_URL = `${HOST}/user/verify-password-reset`;
+
+
+export {
+	axios,
+	SIGN_IN_URL,
+	SIGN_UP_URL,
+	SEND_PASSWORD_RESET_CODE_URL,
+	VERIFIY_PASSWORD_RESET_CODE_URL
+};
