205eec1cd34493bacbe64629bb8e6f91d7b830d9
hophoet <hophoet@gmail.com>
2021-08-18 00:04:37 +0000

    (component) add slide presentation scroll indication component

---
+++ src/components/presentation/PresentationIndicator.tsx
@@ -0,0 +1,44 @@
+import React, {useEffect, useState} from 'react';
+import {TextInput, Image, ScrollView, FlatList, Text, View, StyleSheet, TouchableOpacity, Dimensions} from 'react-native'
+
+
+const PresentationIndicator = ({item, index, scrollX, currentIndex}:any) => {
+    let indicators = [{id:0}, {id:1}, {id:2}]
+	return(
+		<View style={styles.container}>
+            {
+                indicators.map((it, index) => {
+                    return (
+                        <View 
+                            key={index.toString()}
+                            style={[styles.indicator, (it.id!=currentIndex)?styles.indicator2:{}]}>
+                        </View>
+                    )
+                })
+            }
+		</View>
+	)
+
+}
+export default PresentationIndicator;
+
+const {width, height} = Dimensions.get('window');
+const styles = StyleSheet.create({
+	container:{
+        flexDirection:'row',
+		justifyContent:'center',
+		alignItems:'center',
+        paddingVertical:50,
+		//backgroundColor:'red',
+    },
+    indicator:{
+        width:10,
+        height:10,
+        borderRadius:60,
+		backgroundColor:'blue',
+        marginHorizontal:5,
+    },
+    indicator2:{
+        opacity:.3,
+    }
+})
