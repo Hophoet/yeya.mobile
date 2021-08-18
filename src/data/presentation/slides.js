1fc2560f98946ef044858a83dc33a41e04779fac
hophoet <hophoet@gmail.com>
2021-08-18 00:05:35 +0000

    (data) add presentation scroll slide component data

---
+++ src/data/presentation/slides.js
@@ -0,0 +1,23 @@
+export default [
+	{
+		id:'1',
+		title:"Boutique",
+		description:'Creer et gerer votre boutique en ligne pour vos clients potentiels.',
+		image: require('../../assets/images/chat4.png'),
+		icon:'cash-outline',
+	},
+	{
+		id:'3',
+		title:'Discuter',
+		description:'Discuter avec vos clients pour négocier et conclure vos ventes.',
+		image: require('../../assets/images/chat4.png'),
+		icon:'chatbubble-ellipses-outline',
+	},
+	{
+		id:'4',
+		title:'Livrer',
+		description:'Livrer les commandes à vos clients, pour accroite la confiance de votre boutique aux autres clients potentiels.',
+		image: require('../../assets/images/chat4.png'),
+		icon:'scan',
+	},
+]
