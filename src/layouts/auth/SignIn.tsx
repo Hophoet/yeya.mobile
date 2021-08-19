0fa0005ddb73ad4802c24dcb201d2e477de30476
hophoet <hophoet@gmail.com>
2021-08-19 11:36:41 +0000

    (layout) style sign up layout handle the textinput field validation checking handle navigations

--- src/layouts/auth/SignIn.tsx
+++ src/layouts/auth/SignIn.tsx
@@ -1,10 +1,9 @@
 
-import React, {useState} from 'react';
-import {ImageBackground, StatusBar, ScrollView, Text, View, StyleSheet, TouchableOpacity, Dimensions} from 'react-native'
-import Icon from "react-native-vector-icons/Ionicons";
-
+import React from 'react';
+import { ActivityIndicator,  StatusBar, ScrollView, Text, View, StyleSheet, TouchableOpacity, Dimensions} from 'react-native'
 import MainHeader from '../../components/MainHeader';
-import AuthNavHeader from '../../components/AuthNavHeader';
+import Toast from '../../components/toasts'
+import { mailFormatIsValid } from '../../utils/mail'
 import CButton from '../../components/CButton';
 import CTextInput from '../../components/CTextInput';
 import { colors } from '../../assets/colors/main'
@@ -24,7 +23,8 @@ type State = {
 class SignIn extends React.Component<Props, State> {
 
 	_isMounted:boolean;
-	message:string = '';
+	email:string = '';
+	password:string = '';
 	constructor(props:Props) {
 		super(props);
 		// Set the component mount state to false
@@ -33,21 +33,43 @@ class SignIn extends React.Component<Props, State> {
 		this.state = {
 			requestIsLoading:false
 		};
+		this.email = ''
+		this.password = ''
  	}
 
    _customNavHeader = () => {
         this.props.navigation.setOptions({
           header: () => (
             <MainHeader 
-				title="Connectez-vous"
-				description="améliorer l'application en nous laissant votre message l'application en nous laissant votre message"
+				title="Bienvenue"
+				description="je suis si heureux de voir que vous pouvez continuer à vous connecter pour poster vos taches ou retrouver des taches a faire"
 				// firstAction={()=>{}}	
 				navigateTo={()=> {}}
 			/>
-          ),
+		  ),
         });
 
     }
+
+	onSubmit = () => {
+		if(this.email && this.password){
+			if(mailFormatIsValid(this.email)){
+				// can login
+				this.setState({requestIsLoading:true})
+
+			}
+			else if(!mailFormatIsValid(this.email)){
+				Toast._show_bottom_toast('Entrer un email valide');
+			}
+		}
+		else if (!this.email){
+			Toast._show_bottom_toast('Entrer votre adresse email');
+		}
+		else if (!this.password){
+			Toast._show_bottom_toast('Entrer votre mot de passe');
+		}
+
+	}
 
 	componentDidMount() { 
 		// Enable the component mount state
@@ -58,8 +80,8 @@ class SignIn extends React.Component<Props, State> {
 	navigateToSignUp = ()=>{
 		this.props.navigation.navigate('SignUp')
 	}
- 	navigateToResetPassword = ()=>{
-		this.props.navigation.navigate('ResetPassword')
+ 	navigateToSendPasswordResetCode = ()=>{
+		this.props.navigation.navigate('SendPasswordResetCode')
 	}
 
 	navigateToHome = ()=>{
@@ -72,47 +94,64 @@ class SignIn extends React.Component<Props, State> {
 			<View style={styles.container}>
 				<StatusBar 
 					hidden={false}
-					backgroundColor={'black'}
+					barStyle={'dark-content'}
+					backgroundColor={'white'}
 				/>
-				<View
+				<ScrollView
 					style={styles.contentContainer}
 				>
+					{ this.state.requestIsLoading &&
+					<View style={styles.activityIndicatorContainer}>
+						<ActivityIndicator size={'large'} color={colors.main} />
+					</View>
+					}
 					<View style={styles.row1}>
 						<View style={styles.textInputsContainer}>
 							<View style={styles.textInputContainer}>
 								<CTextInput
+									textInputProps={{keyboardType:'email-address'}}
+									autoFocus={true}
 									icon='mail'	
-									placeholder='Email'
+									onChangeText={(text:string)=>this.email=text}
+									onSubmitEditing={()=>this.onSubmit}
+									placeholder='Address-email'
 								/>
 							</View>
 							<View style={styles.textInputContainer}>
 								<CTextInput
 									icon='lock-closed'	
-									placeholder='Password'
+									password
+									focus={true}
+									onSubmitEditing={()=>this.onSubmit}
+									onChangeText={(text:string)=>this.password=text}
+									placeholder='Mot de passe'
 								/>
 							</View>
 						</View>
+						<TouchableOpacity 
+							onPress={this.navigateToSendPasswordResetCode}
+							style={styles.forgotPasswordButton}>
+							<Text style={styles.forgotPasswordButtonLabel}>Mot de passe oublié ?</Text>
+						</TouchableOpacity>
 					</View>
 					<View style={styles.row2}>
 						<CButton 
 							textStyle={{color:'white'}}
-							onPress={this.navigateToHome}	
+							onPress={this.onSubmit}	
 							iconPosition='right'
 							label='Connexion'
 						/>
-						<TouchableOpacity
-							style={styles.signUpButton}	
-						>
-							<Text style={styles.signUpButtonLabel}>Vous n'avez pas de compte yeya ?</Text>
-						</TouchableOpacity>
 
 					</View>
 					<View style={styles.row3}>
-						<TouchableOpacity>
-							<Text>Vous n'avez pas de compte yeya ?</Text>
+						<TouchableOpacity
+							style={styles.signUpButton}	
+							onPress={this.navigateToSignUp}
+						>
+							<Text style={styles.signUpButtonLabel}>Vous n'avez pas de compte yeya ?</Text>
 						</TouchableOpacity>
 					</View>
-				</View>
+				</ScrollView>
 			</View>
 		)
 	}
@@ -184,7 +223,7 @@ const styles = StyleSheet.create({
 		marginHorizontal:width/10,
 	},
 	formBottomContainer:{
-		paddingVertical:10,
+		//paddingVertical:10,
 		justifyContent:'center'
 		
 	},
@@ -196,7 +235,6 @@ const styles = StyleSheet.create({
 	formBottomFirstButton:{
 		flexDirection:'row',
 		alignItems:'center',
-	
 	},
 	footerLabel:{
 		color:'white',
@@ -206,13 +244,21 @@ const styles = StyleSheet.create({
 		fontSize:15,
 	},
 	textInputsContainer:{
-		paddingBottom:20,
+		//paddingBottom:20,
 	},
 	signUpButton:{
 		//backgroundColor:'red',
 		alignItems:'flex-end',
 		justifyContent:'center',
-		paddingVertical:10,
+		paddingVertical:20,
+	},
+	forgotPasswordButton:{
+		alignItems:'flex-end',
+		justifyContent:'center',
+		paddingBottom:20,
+	},
+	activityIncatorContainer:{
+
 	}
 
 })
