6ce4a2d84ef0502de92b48d5833f724e6ad7db00
hophoet <hophoet@gmail.com>
2021-08-19 11:34:05 +0000

    (component) update the custom text input component handle password visibility handle keyboard type from props

--- src/components/CTextInput.tsx
+++ src/components/CTextInput.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import {TextInput, View, StyleSheet, Dimensions} from 'react-native'
+import React, {useState} from 'react';
+import {TextInput, View, StyleSheet, TouchableOpacity, Dimensions} from 'react-native'
 import Icon from "react-native-vector-icons/Ionicons";
 import { colors } from '../assets/colors/main'
 
 
-const CTextInput = ({icon, label, textInputProps, onChangeText, keyboardType, autoFocus, placeholderTextColor, selectionColor, iconColor, textInputStyle, textInputContainerStyle, placeholder}:any) => {
+const CTextInput = ({focus, onSubmitEditing, password, label, textInputProps, onChangeText, keyboardType, autoFocus, placeholderTextColor, selectionColor, iconColor, textInputStyle, textInputContainerStyle, placeholder}:any) => {
+	const [textInputContainerFocusStyle, setTextInputContainerFocusStyle] = useState({})
+	const [passwordVisible, setPasswordVisibility ] = useState(false)
+
+
+	const togglePasswordVisibility  = () => setPasswordVisibility(!passwordVisible)
+
 	return(
 		<View style={styles.container}>
-			<View style={[styles.textInputContainer, textInputContainerStyle]}>
+			<View style={[styles.textInputContainer, textInputContainerStyle, textInputContainerFocusStyle]}>
 				<TextInput
 					{...textInputProps}
+					onFocus={()=>{
+						setTextInputContainerFocusStyle({borderColor:'#5892FF', borderWidth:2})
+					}}
+					onBlur={() => {
+						setTextInputContainerFocusStyle({})
+					}}
 					autoFocus={autoFocus}
+					focus={focus}
+					onSubmitEditing={onSubmitEditing}
 					placeholderTextColor={(placeholderTextColor)?placeholderTextColor:'gray'}
 					selectionColor={(selectionColor)?selectionColor:'white'}
 					style={[styles.textInput, textInputStyle]}
 					placeholder={placeholder}
+					secureTextEntry={ password && !passwordVisible}
 					onChangeText={(text)=>{
 						if(onChangeText){
 							onChangeText(text)
 						}
 					}}
 				/>
+				{ password &&
+				<TouchableOpacity 
+					onPress={togglePasswordVisibility}
+					style={styles.iconContainer}>
+					<Icon name={passwordVisible?'eye-off':'eye'} color='gray' size={20}/>
+				</TouchableOpacity>
+				}
 			</View>
 		</View> 
 	)
@@ -36,17 +58,23 @@ const styles = StyleSheet.create({
 		color:'gray',
 		fontSize:18,
 		// padding:5,
-		flex:1,
-		//backgroundColor:'#0071',
+		flex:6,
 		padding:15,
 	},
 	textInputContainer:{
 		flexDirection:'row',
 		alignItems:'center',
 		borderRadius:5,
-		borderWidth:2,
+		backgroundColor:'#EFF1F2',
+
+		// borderWidth:2,
 		//padding:5,
 		borderColor:'#0002',
+	},
+	iconContainer:{
+		flex:1,
+		justifyContent:'center',
+		alignItems:'center',
 	}
 	
 
