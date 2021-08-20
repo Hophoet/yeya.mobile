46efbb2801416bde7c6c07db1d01017349ea19e9
hophoet <hophoet@gmail.com>
2021-08-20 11:37:12 +0000

    (backend) update SEND_PASSWORD_RESET_CODE url

--- src/backend/requests/setup.tsx
+++ src/backend/requests/setup.tsx
@@ -5,7 +5,7 @@ const HOST ='http://127.0.0.1:8000';//'https://byiapi.herokuapp.com';
 // Authentication urls 
 const SIGN_UP_URL = `${HOST}/auth/register`;
 const SIGN_IN_URL = `${HOST}/auth/jwt/login`;
-const SEND_PASSWORD_RESET_CODE_URL = `${HOST}/user/reset-pasword`;
+const SEND_PASSWORD_RESET_CODE_URL = `${HOST}/user/reset-password`;
 const VERIFIY_PASSWORD_RESET_CODE_URL = `${HOST}/user/verify-password-reset`;
 
 
