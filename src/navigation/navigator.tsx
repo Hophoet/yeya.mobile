b6ce39612bdb7e02929fe879b8a81205f33ab732
hophoet <hophoet@gmail.com>
2021-08-21 23:38:40 +0000

    (navigation) add loader layout as the app enter point component

--- src/navigation/navigator.tsx
+++ src/navigation/navigator.tsx
@@ -3,10 +3,17 @@ import { createStackNavigator } from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
 import {AuthNavigator} from './auth.navigator';
 import MainNavigator from './main.navigator'
+import Loader from '../layouts/splash/Loader'
 const Stack = createStackNavigator();
 
 export const Nav = (): React.ReactElement => (
   <Stack.Navigator>
+    <Stack.Screen 
+			options={{
+				headerShown:false,
+			}}
+      name='Loader' 
+      component={Loader}/>
     <Stack.Screen 
 			options={{
 				headerShown:false,
