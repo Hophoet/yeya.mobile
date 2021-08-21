9cda94b8de54473bde50cd0ffc9c4fffa0bc5f55
hophoet <hophoet@gmail.com>
2021-08-21 19:41:52 +0000

    (backend request) add getJobs to handle all jobs getting request add getUserFavorite to handle user favorite jobs getting

---
+++ src/backend/requests/job.tsx
@@ -0,0 +1,58 @@
+import {
+	axios,
+	GET_JOBS_URL,
+	GET_FAVORITE_JOBS_URL,
+} from './setup';
+
+import { 
+	GetJobsRequestType,
+	GetUserFavoriteJobsRequestType,
+
+} from './types';
+
+
+function getJobs(data:GetJobsRequestType){
+	let authToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiOWI5NWQyM2UtYjQ5ZS00ZjZiLWIxODYtMGRlZTRkYzY5YThiIiwiYXVkIjoiZmFzdGFwaS11c2VyczphdXRoIiwiZXhwIjoxNjMyNTc2NjI4fQ.0WIoEf9dkGcL8QpILKIU9NJVCwk28gR4tbn7rOElfdM'
+	return new Promise( (resolve, reject) => {
+		axios({
+			url: GET_JOBS_URL,
+			method: 'GET',
+			data:{},
+			headers:{
+				Authorization:`Bearer ${authToken}`
+			}
+		})
+		.then((response:any) => {
+			resolve(response);
+		})
+		.catch((error:any) => {
+			reject(error);
+		})
+	})
+}
+
+function getUserFavorite(data:GetUserFavoriteJobsRequestType){
+	let authToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiOTNlZDgyY2MtZmE4Ny00ZWQwLWIwNDktMDczOTU0Yzg3NjIxIiwiYXVkIjoiZmFzdGFwaS11c2VyczphdXRoIiwiZXhwIjoxNjMyNTgxNjMzfQ.Bj-jOMHM3ajKwN2HucwphS8v8m0tfBq2svWDbS9IiUk'
+	return new Promise( (resolve, reject) => {
+		axios({
+			url: GET_FAVORITE_JOBS_URL,
+			method: 'GET',
+			data:{},
+			headers:{
+				Authorization:`Bearer ${authToken}`
+			}
+		})
+		.then((response:any) => {
+			resolve(response);
+		})
+		.catch((error:any) => {
+			reject(error);
+		})
+	})
+}
+
+
+export {
+	getJobs,
+	getUserFavorite
+}
