117629a581ba84d491d4903140bafac490c4184f
hophoet <hophoet@gmail.com>
2021-08-21 19:38:46 +0000

    (backend, setup) add GET_JOBS_URL & GET_FAVORITE_JOBS_URL

--- src/backend/requests/setup.tsx
+++ src/backend/requests/setup.tsx
@@ -8,11 +8,16 @@ const SIGN_IN_URL = `${HOST}/auth/jwt/login`;
 const SEND_PASSWORD_RESET_CODE_URL = `${HOST}/user/reset-password`;
 const VERIFIY_PASSWORD_RESET_CODE_URL = `${HOST}/user/verify-password-reset`;
 
+// job
+const GET_JOBS_URL = `${HOST}/api/v1/jobs`;
+const GET_FAVORITE_JOBS_URL = `${HOST}/api/v1/user/favorite-jobs`;
 
 export {
 	axios,
 	SIGN_IN_URL,
 	SIGN_UP_URL,
+	GET_JOBS_URL,
+	GET_FAVORITE_JOBS_URL,
 	SEND_PASSWORD_RESET_CODE_URL,
 	VERIFIY_PASSWORD_RESET_CODE_URL
 };
