1188215a3092ec767ecdc5901e72129f19860f97
hophoet <hophoet@gmail.com>
2021-08-21 19:40:04 +0000

    (navigation) add Main stack navigator to the main stack navigation

--- src/navigation/navigator.tsx
+++ src/navigation/navigator.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
 import {AuthNavigator} from './auth.navigator';
-// import MainNavigator from './main.navigator'
+import MainNavigator from './main.navigator'
 const Stack = createStackNavigator();
 
 export const Nav = (): React.ReactElement => (
@@ -11,11 +11,16 @@ export const Nav = (): React.ReactElement => (
 			options={{
 				headerShown:false,
 			}}
+      name='Main' 
+      component={MainNavigator}/>
+    <Stack.Screen 
+			options={{
+				headerShown:false,
+			}}
       name='Auth' 
       component={AuthNavigator}/>
   </Stack.Navigator>
 );
-
 
 const Navigator = (): React.ReactElement => (
     <NavigationContainer>
