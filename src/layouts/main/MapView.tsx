f01bed82a90bd68818dc5b9844983267596b7aff
hophoet <hophoet@gmail.com>
2021-08-21 19:35:55 +0000

    (layout) add map view

---
+++ src/layouts/main/MapView.tsx
@@ -0,0 +1,178 @@
+
+import React from 'react';
+import { ActivityIndicator,  StatusBar, ScrollView, Text, View, StyleSheet, TouchableOpacity, Dimensions} from 'react-native'
+import MainHeader from '../../components/MainHeader';
+import Toast from '../../components/toasts'
+import { mailFormatIsValid } from '../../utils/mail'
+import { signIn } from '../../backend/requests/auth'
+import { SignInRequestType } from '../../backend/requests/types'
+import CButton from '../../components/CButton';
+import CTextInput from '../../components/CTextInput';
+import { colors } from '../../assets/colors/main'
+import { connect } from 'react-redux'
+
+type Props = {
+	navigation:any,
+	authUser: any,
+	authUserToken:string,
+	dispatch:any
+}
+
+type State = {
+	requestIsLoading:boolean,
+}
+
+class ListView extends React.Component<Props, State> {
+
+	_isMounted:boolean;
+	email:string = '';
+	password:string = '';
+	constructor(props:Props) {
+		super(props);
+		// Set the component mount state to false
+		this._isMounted = false;
+		// Set the state
+		this.state = {
+			requestIsLoading:false
+		};
+		this.email = ''
+		this.password = ''
+ 	}
+
+
+	componentDidMount() { 
+		// Enable the component mount state
+		this._isMounted = true;
+	}
+
+	navigateToSignUp = ()=>{
+		this.props.navigation.navigate('SignUp')
+	}
+ 	navigateToSendPasswordResetCode = ()=>{
+		this.props.navigation.navigate('SendPasswordResetCode')
+	}
+
+	navigateToHome = ()=>{
+		this.props.navigation.navigate('App')
+	}
+
+
+	render(){
+		return(
+			<View style={styles.container}>
+				<StatusBar 
+					hidden={false}
+					barStyle={'dark-content'}
+					backgroundColor={'white'}
+				/>
+				<Text >HOME SCREEN</Text>
+			</View>
+		)
+	}
+
+}
+//maps with the state global
+const mapDispatchToProps = (dispatch:any) => {
+    return {
+        dispatch: (action:any) => {dispatch(action)}
+    }
+}
+
+const mapStateToProps = (state:any) => {
+    return {
+        authUserToken:state.authUserToken,
+        authUser:state.authUser
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(ListView)
+
+const {width, height} = Dimensions.get('window');
+const styles = StyleSheet.create({
+	container:{
+		flex:1,
+		backgroundColor:'white',
+		//backgroundColor:'#6C63FF'
+	},
+	row1Title:{
+		fontSize:20,
+		fontWeight:'bold',
+		color:'black',
+		// alignSelf:'center',
+	},
+	contentContainer:{
+		flex:1,
+		backgroundColor:'white',
+		paddingHorizontal:20,
+	},
+	row1:{
+		flex:4,
+		paddingTop:20,
+		//backgroundColor:'red',
+		// justifyContent:'center',
+		// justifyContent:'center',
+		// margin:30,
+	},
+	row2:{
+		flex:3,
+		//backgroundColor:'gray',
+		justifyContent:'center',
+	},
+	row3:{
+		flex:1,
+		//backgroundColor:'gray',
+		alignItems:'center',
+		justifyContent:'center',
+	},
+	textInputContainer:{
+		paddingBottom:10,
+	},
+	
+	button:{
+		flexDirection:'row',
+		justifyContent:'center',
+		alignItems:'center',
+		backgroundColor:'red',
+		padding:10,
+		borderRadius:width/2,
+		marginHorizontal:width/10,
+	},
+	formBottomContainer:{
+		//paddingVertical:10,
+		justifyContent:'center'
+		
+	},
+	formBottomFirstLabel:{
+		color:'black',	
+		fontWeight:'bold',
+		textTransform:'uppercase',
+	},
+	formBottomFirstButton:{
+		flexDirection:'row',
+		alignItems:'center',
+	},
+	footerLabel:{
+		color:'white',
+	},
+	footerLabelRight:{
+		fontWeight:'bold',
+		fontSize:15,
+	},
+	textInputsContainer:{
+		//paddingBottom:20,
+	},
+	signUpButton:{
+		//backgroundColor:'red',
+		alignItems:'flex-end',
+		justifyContent:'center',
+		paddingVertical:20,
+	},
+	forgotPasswordButton:{
+		alignItems:'flex-end',
+		justifyContent:'center',
+		paddingBottom:20,
+	},
+	activityIncatorContainer:{
+
+	}
+
+})
