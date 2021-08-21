7a3bbc915e5239ab226e19e6b155b36dcba3d3b8
hophoet <hophoet@gmail.com>
2021-08-21 23:32:23 +0000

    (backend setup) add TOGGLE_JOB_FAVORITE_URL to handle job favorite handling

--- src/backend/requests/setup.tsx
+++ src/backend/requests/setup.tsx
@@ -11,6 +11,7 @@ const VERIFIY_PASSWORD_RESET_CODE_URL = `${HOST}/user/verify-password-reset`;
 // job
 const GET_JOBS_URL = `${HOST}/api/v1/jobs`;
 const GET_FAVORITE_JOBS_URL = `${HOST}/api/v1/user/favorite-jobs`;
+const TOGGLE_JOB_FAVORITE_URL = `${HOST}/api/v1/job/toggle-favorite`;
 
 export {
 	axios,
@@ -19,5 +20,6 @@ export {
 	GET_JOBS_URL,
 	GET_FAVORITE_JOBS_URL,
 	SEND_PASSWORD_RESET_CODE_URL,
-	VERIFIY_PASSWORD_RESET_CODE_URL
+	VERIFIY_PASSWORD_RESET_CODE_URL,
+	TOGGLE_JOB_FAVORITE_URL,
 };
