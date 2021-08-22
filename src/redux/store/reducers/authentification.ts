30a90db842e548f5884a74bcf51a91391bdd938d
hophoet <hophoet@gmail.com>
2021-08-22 00:06:57 +0000

    (redux) set reducer cases

--- src/redux/store/reducers/authentification.ts
+++ src/redux/store/reducers/authentification.ts
@@ -2,10 +2,25 @@ import {
 		CLEAN, 
 		SET_AUTH_USER_TOKEN,
 		SET_AUTH_USER,
+		SET_CATEGORIES,
+		SET_CHATS,
+		SET_CITIES,
+		SET_JOBS,
+		SET_PROPOSALS,
+		SET_FAVORITES,
 		
 } from '../actions';
 
-const intialState:any = {  presentationRead:false };
+const intialState:any = {
+	jobs:[],
+	categories:[],
+	cities:[],
+	proposals:[],
+	favorites:[],
+	chats:[],
+	presentationRead:false,
+
+};
 
 
 const reducer = (state = intialState, action:any) => {
@@ -28,10 +43,58 @@ const reducer = (state = intialState, action:any) => {
 		};
 		return newState;
 
+    case SET_JOBS:
+		const jobs:any[] = action.value;
+		// console.log(state)
+		newState = {
+			...state,
+			jobs: jobs,
+		};
+		return newState;
+
+    case SET_CHATS:
+		const chats:any[] = action.value;
+		newState = {
+			...state,
+			chats: chats,
+		};
+		return newState;
+
+    case SET_CATEGORIES:
+		const categories:any[] = action.value;
+		newState = {
+			...state,
+			categories: categories,
+		};
+		return newState;
+
+    case SET_CITIES:
+		const cities:any[] = action.value;
+		newState = {
+			...state,
+			cities: cities,
+		};
+		return newState;
+
+    case SET_PROPOSALS:
+		const proposals:any[] = action.value;
+		newState = {
+			...state,
+			proposals: proposals,
+		};
+		return newState;
+
+    case SET_FAVORITES:
+		const favorites:any[] = action.value;
+		newState = {
+			...state,
+			favorites: favorites,
+		};
+		return newState;
+
 	case CLEAN:
 		newState = {}
 		return newState
-		
 		
     default:
 		return state;
