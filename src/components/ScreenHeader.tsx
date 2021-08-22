bd564c3c43a226f9b59d72c645a32a56edc06565
hophoet <hophoet@gmail.com>
2021-08-22 00:00:45 +0000

    (component) add screen header component

---
+++ src/components/ScreenHeader.tsx
@@ -0,0 +1,60 @@
+
+import React from 'react'
+import { Text, StyleSheet, View, Dimensions, Image, TouchableOpacity} from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons'
+
+
+
+
+type Props = {
+	title:string,
+	description?:string
+}
+
+const ScreenHeader = ({title, description}:Props) => {
+	return(
+		<View style={styles.container}>
+			<View style={styles.row1}>
+				<View style={styles.headerContainer}>
+					<Text style={styles.headerTitle}>{title}</Text>
+				</View>
+				<View style={styles.naContainer}>
+					<Text style={styles.description}>{description}</Text>
+				</View>
+			</View>
+		</View>
+	)
+}
+
+export default ScreenHeader;
+
+const {width, height} = Dimensions.get('window');
+const styles = StyleSheet.create({
+	container:{
+		//backgroundColor:'red',
+		// height:width/8,
+		// backgroundColor:'red',
+		paddingHorizontal:20,
+	},
+	headerContainer:{
+		// backgroundColor:'red',
+		// paddingHorizontal:20,
+		// paddingBottom:10,
+		borderBottomWidth:StyleSheet.hairlineWidth,
+		// borderBottomColor:'black',
+		// marginBottom:10,
+  },
+   headerTitle:{
+		color:'black',
+		fontSize:30,
+		fontWeight:'bold',
+		paddingVertical:10,
+	},
+	description:{
+		color:'gray',
+	},
+	row1:{
+		paddingVertical:10,
+		// backgroundColor:'red',
+	},
+	})
