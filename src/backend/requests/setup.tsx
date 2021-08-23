fdd3a2162c7d0b9f3db540bafb4d71e8fce584bb
hophoet <hophoet@gmail.com>
2021-08-23 21:31:34 +0000

    (backend, url) add backend request urls SEND_CHANGE_MESSAGES_URL CET_USER_CHAT_CONVERSATIONS_URL GET_CHAT_CONVERSATION_URL

--- src/backend/requests/setup.tsx
+++ src/backend/requests/setup.tsx
@@ -13,7 +13,13 @@ const GET_JOBS_URL = `${HOST}/api/v1/jobs`;
 const GET_FAVORITE_JOBS_URL = `${HOST}/api/v1/user/favorite-jobs`;
 const TOGGLE_JOB_FAVORITE_URL = `${HOST}/api/v1/job/toggle-favorite`;
 
+// Chat
+const SEND_CHAT_MESSAGE_URL = `${HOST}/api/v1/chat/send-message`;
+const GET_USER_CHAT_CONVERSATIONS_URL = `${HOST}/api/v1/chat/user/conversations`;
+const GET_CHAT_CONVERSATION_URL = `${HOST}/api/v1/chat/conversation`;
+
 export {
+	HOST,
 	axios,
 	SIGN_IN_URL,
 	SIGN_UP_URL,
@@ -22,4 +28,7 @@ export {
 	SEND_PASSWORD_RESET_CODE_URL,
 	VERIFIY_PASSWORD_RESET_CODE_URL,
 	TOGGLE_JOB_FAVORITE_URL,
+	SEND_CHAT_MESSAGE_URL,
+	GET_USER_CHAT_CONVERSATIONS_URL,
+	GET_CHAT_CONVERSATION_URL,
 };
