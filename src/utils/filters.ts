6b7d987b005be9509d63964d860ae582371bbc3b
hophoet <hophoet@gmail.com>
2021-09-10 13:04:28 +0000

    (utils) add sortJobsByCategories to handle job filter by categories util function

--- src/utils/filters.ts
+++ src/utils/filters.ts
@@ -31,6 +31,7 @@ function sortByMostRecent(jobs:any[]){
                 // job created timestamp
                 let j1ts = Math.floor(new Date(job1.created_at).valueOf()/1000);
                 let j2ts = Math.floor(new Date(job2.created_at).valueOf()/1000);
+                // console.log('exists', j1ts, j2ts)
                 if(j2ts > j1ts){
                     return 4;
                 }
@@ -76,10 +77,34 @@ function sortByMostPopular(jobs:any[]){
     return jobs;
 }
 
+function sortJobsByCategories(jobs:any[], baseJobs:any[], selectedCategories:any[]){	
+	let sortedJobs:any[] = [];
+	baseJobs.map(job => {
+		let categoryRelatedToTheJob = selectedCategories.find(category=>{
+			return category.id==job.category.id;
+		});
+		if(categoryRelatedToTheJob){
+			sortedJobs.push(job);
+		}
+	})	
+	//
+	if(selectedCategories.length > 0){
+		let lastSelectedCategory = selectedCategories[selectedCategories.length-1]
+		sortedJobs = sortedJobs.sort((a, b) => {
+			if(lastSelectedCategory.id == a.category.id){
+				return -1;
+			}	
+			return 0;
+		})
+	}
+	return sortedJobs;
+}
+
 
 
 export {
     searchQuery,
     sortByMostRecent,
-    sortByMostPopular
+    sortByMostPopular,
+    sortJobsByCategories,
 }
