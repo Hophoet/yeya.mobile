42f492498d17e2e76316af20d72a5f22256f72b9
hophoet <hophoet@gmail.com>
2021-09-18 15:27:39 +0000

    (refactory) remove unused imports

--- src/components/proposal/ProposalsItem.tsx
+++ src/components/proposal/ProposalsItem.tsx
@@ -1,11 +1,9 @@
 
-import React, {useState} from 'react'
+import React from 'react'
 import {Image, StyleSheet, View , Text, Dimensions, TouchableOpacity} from 'react-native'
 import Icon from 'react-native-vector-icons/Ionicons'
 import { colors } from '../../assets/colors/main'
 import moment from 'moment';
-import { toggleJobFavorite } from '../../backend/requests/job'
-import { ToggleJobFavoriteRequestType } from '../../backend/requests/types'
 import { connect } from 'react-redux'
 
 type Props = {
