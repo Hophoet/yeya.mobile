c34ffd6078c79edaa13396498c452248f8a22605
hophoet <hophoet@gmail.com>
2021-09-18 15:26:49 +0000

    (refactory) remove unused imports

--- src/components/presentation/PresentationIndicator.tsx
+++ src/components/presentation/PresentationIndicator.tsx
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from 'react';
-import {TextInput, Image, ScrollView, FlatList, Text, View, StyleSheet, TouchableOpacity, Dimensions} from 'react-native'
+import React from 'react';
+import {View, StyleSheet, Dimensions} from 'react-native'
 import {colors} from '../../assets/colors/main'
 
 
