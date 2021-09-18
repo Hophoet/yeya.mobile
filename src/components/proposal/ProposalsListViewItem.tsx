f1f511a311935ff0bb847d4cf0b603362433216f
hophoet <hophoet@gmail.com>
2021-09-18 15:27:55 +0000

    (refactory) remove unused imports

--- src/components/proposal/ProposalsListViewItem.tsx
+++ src/components/proposal/ProposalsListViewItem.tsx
@@ -1,8 +1,6 @@
 
-import React, {useState} from 'react'
-import {Image, FlatList, StyleSheet, View , Text, Dimensions, TouchableOpacity} from 'react-native'
-import Icon from 'react-native-vector-icons/Ionicons'
-import { colors } from '../../assets/colors/main'
+import React from 'react'
+import { FlatList, StyleSheet, View , Text, Dimensions, TouchableOpacity} from 'react-native'
 import moment from 'moment';
 import { toggleJobFavorite } from '../../backend/requests/job'
 import { ToggleJobFavoriteRequestType } from '../../backend/requests/types'
